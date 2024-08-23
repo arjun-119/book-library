3f04cdcbd238a6044a6c89c19678e103af70d3a8
Arjun Bharadwaj <arjunkrish2003@gmail.com>
2024-08-23 14:23:15 +0530

    Add users book into the library array.

--- Script.js
+++ Script.js
@@ -1,22 +1,31 @@
-const content_area = document.querySelector("#content");
 const myLibrary = [];
 
-function Book(title, author, pages, read){
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;    
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
 }
 
-const defaultBook1 = new Book("The dance of the Dragons", "Jon Snow", 91, "not read");
+const defaultBook1 = new Book(
+  "The dance of the Dragons",
+  "Jon Snow",
+  91,
+  "not read"
+);
 myLibrary.push(defaultBook1);
 display_books();
 
-function display_books(){
-
+function display_books() {
+    
 }
 
 function addBookToLibrary() {
+  const title = document.querySelector("title");
+  const author = document.querySelector("author");
+  const pages = document.querySelector("pages");
+  const read = document.querySelector("read");
 
+  const book1 = new Book(title, author, pages, read);
+  myLibrary.push(book1);
 }
-
