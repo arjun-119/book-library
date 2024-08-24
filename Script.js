94d5e5c5e8d7b5d3d34c15cf52ac6fab542bf2f5
Arjun Bharadwaj <arjunkrish2003@gmail.com>
2024-08-25 00:47:46 +0530

    Read through the array of book objects and display it to the frontend
    create a function to iterate over the objects in the array and display them dynamically into the frontend.

--- Script.js
+++ Script.js
@@ -1,4 +1,5 @@
 const myLibrary = [];
+const display_container = document.querySelector(".content");
 
 function Book(title, author, pages, read) {
   this.title = title;
@@ -17,7 +18,30 @@ myLibrary.push(defaultBook1);
 display_books();
 
 function display_books() {
-    
+    myLibrary.forEach(book=>{
+
+      const card = document.createElement('div');
+      card.className = "card";
+
+      const title = document.createElement('div');
+      title.textContent = `Title: ${book.title}`;
+
+      const author = document.createElement('div');
+      author.textContent = `Author: ${book.author}`;
+
+      const pages = document.createElement('div');
+      pages.textContent = `Pages: ${book.pages}`;
+
+      const read_status = document.createElement('div');
+      read_status.textContent = `Read Status: ${book.read}`;
+
+      card.appendChild(title);
+      card.appendChild(author);
+      card.appendChild(pages);
+      card.appendChild(read_status);
+      
+      display_container.appendChild(card);
+    })
 }
 
 function addBookToLibrary() {
