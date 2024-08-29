c7966a054dd293be26853fc7f32df6042a6a0ee5
Arjun Bharadwaj <arjunkrish2003@gmail.com>
2024-08-29 13:45:56 +0530

    Fix the issue with changing the read status on click.

--- Script.js
+++ Script.js
@@ -8,40 +8,39 @@ function Book(title, author, pages, read) {
   this.read = read;
 }
 
-const defaultBook1 = new Book(
-  "The dance of the Dragons",
-  "Jon Snow",
-  91,
-  "Read"
-);
+const defaultBook1 = new Book("The dance of the Dragons", "Jon Snow", 91, true);
 myLibrary.push(defaultBook1);
 display_books();
 
 function display_books() {
-    myLibrary.forEach(book=>{
+  myLibrary.forEach((book) => {
 
-      const card = document.createElement('div');
-      card.className = "card";
+    const card = document.createElement("div");
+    card.className = "card";
 
-      const title = document.createElement('div');
-      title.textContent = `Title: ${book.title}`;
+    const title = document.createElement("div");
+    title.textContent = `Title: ${book.title}`;
 
-      const author = document.createElement('div');
-      author.textContent = `Author: ${book.author}`;
+    const author = document.createElement("div");
+    author.textContent = `Author: ${book.author}`;
 
-      const pages = document.createElement('div');
-      pages.textContent = `Pages: ${book.pages}`;     
+    const pages = document.createElement("div");
+    pages.textContent = `Pages: ${book.pages}`;
 
-      const read_status = document.createElement('div');
-      read_status.textContent = `Read Status: ${book.read}`;
+    const read_status = document.createElement("button");
+    read_status.textContent = book.read ? "Read" : "Not read";
 
-      card.appendChild(title);
-      card.appendChild(author);
-      card.appendChild(pages);
-      card.appendChild(read_status);
-      
-      display_container.appendChild(card);
+    read_status.addEventListener("click", ()=>{
+      book.read = !book.read;
+      read_status.textContent = book.read ? "Read" : "Not read";
     })
+    card.appendChild(title);
+    card.appendChild(author);
+    card.appendChild(pages);
+    card.appendChild(read_status);
+
+    display_container.appendChild(card);
+  });
 }
 
 function addBookToLibrary(title, author, pages, read) {
@@ -49,45 +48,37 @@ function addBookToLibrary(title, author, pages, read) {
   myLibrary.push(new_book);
 }
 
-const dialog = document.querySelector("dialog")
+const dialog = document.querySelector("dialog");
 const add_button = document.querySelector("#add");
 const close_button = document.querySelector("#close_button");
 const submit_button = document.querySelector("#submit_button");
 const form = dialog.querySelector("form");
 
-add_button.addEventListener("click", ()=>{
+add_button.addEventListener("click", () => {
   dialog.showModal();
 });
 
-close_button.addEventListener("click", ()=>{
+close_button.addEventListener("click", () => {
   dialog.close();
-  });
+});
 
-form.addEventListener("submit", (event)=>{
+form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   const formData = new FormData(form);
-  let status = formData.get("read") === "on";
 
-  if(status === true)
-    status = "Read";
-  
-  else
-    status = "Not read";
-
-  const add_book = new Book(formData.get("title"), formData.get("author"),
-  formData.get("pages"), status);
+  const add_book = new Book(
+    formData.get("title"),
+    formData.get("author"),
+    formData.get("pages"),
+    formData.get("read") === "on"
+  );
   myLibrary.push(add_book);
 
   dialog.close();
   display_container.innerHTML = "";
   display_books();
-  
 
   form.reset();
-    
- } )
+});
 
-
-
- 
